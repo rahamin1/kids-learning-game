9bcf76b294e2ec9c658c233da584a4b88e59024a
rahamin1 <yossi.glass@gmail.com>
2026-07-25 23:12:04 +0300

    Release version 0.1.28

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "brightwood-pwa-local199";
+const CACHE_NAME = "brightwood-pwa-local200";
 const CORE_FILES = [
   "./",
   "./index.html",
@@ -6,10 +6,10 @@ const CORE_FILES = [
   "./remove-updates.html",
   "./updates-pending.html",
   "./updates-confirmed.html",
-  "./styles.css?build=local199",
-  "./games.js?build=local199",
-  "./app.js?build=local199",
-  "./manifest.webmanifest?v=0.1.27",
+  "./styles.css?build=local200",
+  "./games.js?build=local200",
+  "./app.js?build=local200",
+  "./manifest.webmanifest?v=0.1.28",
   "./assets/app-icon-192.png",
   "./assets/app-icon-512.png",
   "./assets/app-icon-star-forest.png",
