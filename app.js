e699b5b1794a252cd32b43beab7cbbd6f41fc9e9
rahamin1 <yossi.glass@gmail.com>
2026-08-08 08:55:14 +0300

    Release 0.1.42: replace krill in food chains

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "0.1.41";
+const APP_VERSION = "0.1.42";
 const FORMSPREE_ENDPOINT = "https://formspree.io/f/xgojggkr";
 const UPDATES_SIGNUP_PAGE = "updates.html";
 const GA_MEASUREMENT_ID = "G-GYG1ZSCPN6";
