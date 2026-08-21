dd4c7713bd1e8ecd3506359d47a24c09ee3a440e
rahamin1 <yossi.glass@gmail.com>
2026-08-21 18:09:37 +0300

    Release 0.2.2: add English landing card

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "0.2.1";
+const APP_VERSION = "0.2.2";
 const FORMSPREE_ENDPOINT = "https://formspree.io/f/xgojggkr";
 const UPDATES_SIGNUP_PAGE = "updates.html";
 const GA_MEASUREMENT_ID = "G-GYG1ZSCPN6";
